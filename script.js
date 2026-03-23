fd0709e37123bdae4b1a161602029b32818f0ce2
Jibanjyoti Tayung <jibansnewlife@gmail.com>
2026-03-23 17:42:58 +0530

    Alpha v0.2.0 localStorage save

--- script.js
+++ script.js
@@ -1,11 +1,23 @@
 let streak = 0;
 
+// load saved streak when page opens
+if (localStorage.getItem("streak")) {
+    streak = parseInt(localStorage.getItem("streak"));
+}
+
+document.getElementById("streak").innerText = streak;
+
 function increaseStreak() {
     streak++;
-    document.getElementById("streak").innerText = streak;
+    saveStreak();
 }
 
 function resetStreak() {
     streak = 0;
+    saveStreak();
+}
+
+function saveStreak() {
+    localStorage.setItem("streak", streak);
     document.getElementById("streak").innerText = streak;
 }
